4bcc0c2e6107ef1b854c07ef45cb8d1dd6eca0c0
Zane Harvell <zane.harvell@gmail.com>
2020-10-26 19:11:44 -0500

    backend done

---
+++ server/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+// const bodyParser = require('body-parser');
+
+app.use(express.static('server/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const history = [1, 2, 3];
+
+app.post('/api/calc', (req, res) => {
+  const val1 = parseInt(req.body.val1);
+  const val2 = parseInt(req.body.val2);
+  let answer = 0;
+  const op = req.body.op;
+
+  if (!val1 || !val2) return res.send(400);
+
+  if (op === 'add') {
+    answer = val1 + val2;
+  } else if (op === 'sub') {
+    answer = val1 - val2;
+  } else if (op === 'mul') {
+    answer = val1 * val2;
+  } else if (op === 'div') {
+    answer = val1 / val2;
+  } else {
+    return res.sendStatus(400);
+  }
+
+  const equation = {
+    val1,
+    val2,
+    op,
+    answer,
+  };
+  history.push(equation);
+
+  //switch(op) {
+  //  case "add":
+  //  answer = val1 + val2;
+
+  ///break;
+  //case "sub":
+  //answer = val1 - val2;
+
+  ///break;
+  //case "mul":
+  //answer = val1 * val2;
+
+  //break;
+  //case "div":
+  //answer = val1 / val2;
+
+  //break;
+  //}
+
+  res.send(200);
+});
+
+app.get('/api/history', (req, res) => {
+  res.send(history);
+});
+
+app.listen(PORT, () => {
+  console.log('server is running on port: ${PORT}');
+});
